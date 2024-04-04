3184a6d6a48e880a7b60aa80d3a6e36ca286bf1e
Shoaib Raza <89098029+shoaibpki@users.noreply.github.com>
2024-04-04 16:29:26 +0100

    added firebase config file

---
+++ fireBaseSetting.ts
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBJ3HhGV5wROBuKcCsYU5JibOmn32_pdds",
+    authDomain: "foodbox-6e3ab.firebaseapp.com",
+    databaseURL: "https://foodbox-6e3ab-default-rtdb.firebaseio.com",
+    projectId: "foodbox-6e3ab",
+    storageBucket: "foodbox-6e3ab.appspot.com",
+    messagingSenderId: "1023826424427",
+    appId: "1:1023826424427:web:5e845115d9d8fb8d1c372e"
+  };
